09c44cfa5e1911051e4e633642e9b4eb8821947a
mleonardo2930 <ing.mleonardo@gmail.com>
2015-02-12 10:43:47 -0700

    Modified send_email.py to print ip address of the host.

--- src/send_email.py
+++ src/send_email.py
@@ -1,6 +1,10 @@
 # Import smtplib for the actual sending function
 import smtplib
 import time
+import socket
+
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
 
 username = 'ing.mleonardo'
 password = 'ML092986'
@@ -8,17 +12,35 @@ password = 'ML092986'
 
 fromaddr = 'ing.mleonardo@gmail.com'
 toaddrs  = 'ing.mleonardo@gmail.com'
-msg = 'Raspberry01 - All Systems Working'
+
+msg = MIMEMultipart()
+
+msg['From'] = fromaddr
+msg['To'] = toaddrs
+msg['Subject'] = 'Water Monitor'
+#body = 'Raspberry04 - All Systems Working at MBB'
+
+#msg.attach(MIMEText(body, 'plain'))
 
 while True:
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("gmail.com",80))
+    ip = s.getsockname()[0]
+    print(ip)
+    s.close()
+    body = 'Raspberry04 - All Systems Working at MBB %s' %ip
+    msg.attach(MIMEText(body, 'plain'))
     
     server = smtplib.SMTP("smtp.gmail.com:587")
+
 
     server.starttls()
     
     server.login(username,password)
-
-    server.sendmail(fromaddr, toaddrs, msg)
+    
+    print body
+    server.sendmail(fromaddr, toaddrs, msg.as_string())
 
     server.quit()
 

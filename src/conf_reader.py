86ebffb419058d86b1a57008a27e3c3d909a9cfc
Miguel Leonardo <ing.mleonardo@gmail.com>
2015-02-18 12:35:46 -0700

    Added changes

--- src/conf_reader.py
+++ src/conf_reader.py
@@ -12,7 +12,10 @@ try:
     Port4_name = itemlist[5].attributes['value'].value
     Site_description = itemlist[6].attributes['value'].value
     Time_support = itemlist[7].attributes['value'].value
-    print Time_support
+    Data_directory = itemlist[8].attributes['value'].value
+    
+    #This section is used for debug
+    #print Port3_name
 except:
 
     print "Error or no configuration file found. Please create one following the default format and try again."
@@ -26,4 +29,4 @@ Conv_factor2 = 0.01
 Conv_factor3 = 0.01
 Conv_factor4 = 0.01
 
-print 'Configuration done.'
+#print 'Configuration done.'
--- src/send_email.py
+++ src/send_email.py
@@ -2,47 +2,86 @@
 import smtplib
 import time
 import socket
+import thread
 
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 
-username = 'ing.mleonardo'
-password = 'ML092986'
+from conf_reader import *
 
 
-fromaddr = 'ing.mleonardo@gmail.com'
-toaddrs  = 'ing.mleonardo@gmail.com'
 
-msg = MIMEMultipart()
+class system_monitor():
 
-msg['From'] = fromaddr
-msg['To'] = toaddrs
-msg['Subject'] = 'Water Monitor'
-#body = 'Raspberry04 - All Systems Working at MBB'
+    def __init__(self, username = 'ing.mleonardo',password = 'ML092986',fromaddr = 'ing.mleonardo@gmail.com', toaddrs  = 'ing.mleonardo@gmail.com',subject = 'Water Monitor', system_name = Datalogger_name , site_name = Site_name):
 
-#msg.attach(MIMEText(body, 'plain'))
+        #Initializing parameters
+        self._username = username
+        self._password = password
+        self._fromaddr = fromaddr
+        self._toaddrs  = toaddrs
+        self._subject = subject
+        self._system_name = system_name
+        self._site_name = site_name
+        self._monitor_interval = 3600 #Monitor interval in seconds
 
-while True:
+        
 
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    s.connect(("gmail.com",80))
-    ip = s.getsockname()[0]
-    print(ip)
-    s.close()
-    body = 'Raspberry04 - All Systems Working at MBB %s' %ip
-    msg.attach(MIMEText(body, 'plain'))
-    
-    server = smtplib.SMTP("smtp.gmail.com:587")
+        self._start = True
+        
+        #Start email report
+        self._thread = thread.start_new_thread(self.run,())
+ 
+        pass
+
+    def run(self):
+
+        while self._start:
+            self.send_email()
+            time.sleep(self._monitor_interval)
+
+    def send_email(self):
+        #Creating message
+        #Connecting to google server
+        self._server = smtplib.SMTP("smtp.gmail.com:587")
+
+        self._server.starttls()
+        
+        self._server.login(self._username,self._password)
+        
+        msg = MIMEMultipart()
+        msg['From'] = self._fromaddr
+        msg['To'] = self._toaddrs
+        msg['Subject'] = self._subject
+        body = '%s' %self._system_name + '- All Systems Working at %s' %self._site_name + ' %s ' %self.get_ip()
+        msg.attach(MIMEText(body, 'plain'))
+
+        #Sending message if connection available
+        self._server.sendmail(self._fromaddr, self._toaddrs, msg.as_string())
+
+    def get_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("gmail.com",80))
+        ip = s.getsockname()[0]
+        #print(ip)
+        s.close()
+        return ip
+
+    def __del__(self):
+        self._server.quit()
+        self._thread.exit_thread()
+        
+        pass
 
 
-    server.starttls()
-    
-    server.login(username,password)
-    
-    print body
-    server.sendmail(fromaddr, toaddrs, msg.as_string())
 
-    server.quit()
+def main():
 
-    time.sleep(3600)
+    system_monitor()
+    #thread.start_new_thread(system_monitor,())
+    #print 'Done'
+    #sm.send_email()
 
+    pass
+
+main()

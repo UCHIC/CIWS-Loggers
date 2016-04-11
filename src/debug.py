649b3fdbd27a7b8e5c870e5ff193a2b2dee051ba
Miguel Leonardo <miguel@avyon.com>
2016-04-11 10:05:17 -0600

    Deleted all code to update with new source code

--- src/debug.py
+++
@@ -1,7 +0,0 @@
-import pyfpdf
- 
-pdf = pyfpdf.FPDF(format='letter')
-pdf.add_page()
-pdf.set_font("Arial", size=12)
-pdf.cell(200, 10, txt="Welcome to Python!", align="C")
-pdf.output("tutorial.pdf")

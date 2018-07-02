aeb7c3fc3e3965c15ac8b09ccc91f3936af8dda6
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2018-07-02 10:21:34 -0600

    Add files via upload

---
+++ hardwaredesign/HybridPrototype/Software/RPi_PythonScripts/haltsig.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+# File:    haltsig.py
+# Authors: Joshua Tracy and Daniel Henshaw
+# Date:    06/05/2018
+# 
+# Description: This tiny program sets GPIO pin 26 as an output and sets it high.
+# Why: This is to create a halt notification signal that our microcontroller
+#	can detect to allow for safe power off.
+#
+
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+pin = 26
+GPIO.setup(pin,GPIO.OUT)
+GPIO.output(pin,GPIO.HIGH)

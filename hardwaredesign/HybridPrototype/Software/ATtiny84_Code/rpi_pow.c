a7720c43f475f6246f392fce88ead3f687587285
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2018-07-06 12:15:13 -0600

    Update rpi_pow.c
    This file was not updated from the version which used an NMOS Transistor. Updates reflect usage of a PMOS Transistor.

--- hardwaredesign/HybridPrototype/Software/ATtiny84_Code/rpi_pow.c
+++ hardwaredesign/HybridPrototype/Software/ATtiny84_Code/rpi_pow.c
@@ -10,7 +10,7 @@ void rpi_ioinit(void)
 	DDRA &= ~(1 << PA1);		// PA1 input  -- PA1 is connected to a button, which the user will push in order to manually turn on the Raspberry Pi and keep it on in order to retrieve datalog files.
 	DDRA |= (1 << PA3);		// PA3 output -- PA3 is connected to a GPIO on the Raspberry Pi. This signal is set on a manual button press, to tell the Raspberry Pi not to shut itself off as it normally does.
 	DDRA &= ~(1 << PA5);		// PA5 input  -- PA5 is connected to a GPIO on the Raspberry Pi. This signal tells the ATtiny whether or not the EEPROM is free to be written.
-	PORTA &= ~(1 << PA7);		// Start Raspberry Pi off
+	PORTA |= (1 << PA7);		// Start Raspberry Pi off (Signal Active LOW)
 	PORTA &= ~(1 << PA3);		// Start "Stay On" Signal off
 	asm("nop");
 	return;
@@ -18,7 +18,7 @@ void rpi_ioinit(void)
 
 void rpi_on(void)
 {
-	PORTA |= (1 << PA7);		// Turn on RPi
+	PORTA &= ~(1 << PA7);		// Turn on RPi
 	_delay_ms(5000);
 	while(!(PINA & 0x4));		// Wait while RPi signal is low
 	return;
@@ -29,7 +29,7 @@ void rpi_off(void)
 	while(PINA & 0x4);			// Wait while RPi signal is high
 	_delay_ms(5000);			// Wait 10 seconds
 	_delay_ms(5000);
-	PORTA &= ~(1 << PA7);		// Turn off RPi
+	PORTA |= (1 << PA7);		// Turn off RPi
 	
 	return;
 }

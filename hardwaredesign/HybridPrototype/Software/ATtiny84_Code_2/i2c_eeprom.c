a1e2d640df61f99db7e30b302e940c5b6571d8ed
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2018-08-02 15:28:59 -0600

    Update i2c_eeprom.c

--- hardwaredesign/HybridPrototype/Software/ATtiny84_Code_2/i2c_eeprom.c
+++ hardwaredesign/HybridPrototype/Software/ATtiny84_Code_2/i2c_eeprom.c
@@ -5,12 +5,12 @@
 void EEPROM_write(unsigned char chip_addr, unsigned int addr, unsigned char* data, unsigned int data_size)
 {
 	unsigned int i;
-	unsigned char data_buffer[data_size + 3];						// Setup new data buffer
-	unsigned char block = (addr >> 13) & 0x8;						// Isolate the Block select bit
+	unsigned char data_buffer[data_size + 3];					// Setup new data buffer
+	unsigned char block = (addr >> 13) & 0x8;					// Isolate the Block select bit
 	data_buffer[0] = (chip_addr << 1) + WRITE + block;				// Load data buffer with I2C address and Write bit
-	data_buffer[1] = (addr >> 8) & 0x00FF;							// Load data buffer with EEPROM memory address
+	data_buffer[1] = (addr >> 8) & 0x00FF;						// Load data buffer with EEPROM memory address
 	data_buffer[2] = (addr & 0x00FF);
-	for (i = 3; i < data_size + 3; i++)							// Load data buffer
+	for (i = 3; i < data_size + 3; i++)						// Load data buffer
 	{
 		data_buffer[i] = data[i - 3];
 	}
@@ -22,12 +22,12 @@ void EEPROM_read(unsigned char chip_addr, unsigned int addr, unsigned char* data
 {
 	unsigned int i;
 	EEPROM_write(chip_addr, addr, data, 0);						// Transmit Address to read at
-	char data_buffer[data_size + 1];								// Setup new data buffer
-	data_buffer[0] = (chip_addr << 1) + READ;						// Load data buffer with I2C address and Read bit
+	char data_buffer[data_size + 1];						// Setup new data buffer
+	data_buffer[0] = (chip_addr << 1) + READ;					// Load data buffer with I2C address and Read bit
 	
-	USI_I2C_Master_Start_Transmission(data_buffer, data_size + 1);	// Transmit Read request, Read EEPROM
+	USI_I2C_Master_Start_Transmission(data_buffer, data_size + 1);			// Transmit Read request, Read EEPROM
 	
-	for (i = 1; i < data_size + 1; i++)							// Load data buffer
+	for (i = 1; i < data_size + 1; i++)						// Load data buffer
 	{
 		data[i - 1] = data_buffer[i];
 	}

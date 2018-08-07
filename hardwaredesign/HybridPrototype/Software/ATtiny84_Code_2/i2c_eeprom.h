631071cc14aa3292fa7e0ae3368cc0da724dec35
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2018-08-07 10:04:36 -0600

    Update i2c_eeprom.h

--- hardwaredesign/HybridPrototype/Software/ATtiny84_Code_2/i2c_eeprom.h
+++ hardwaredesign/HybridPrototype/Software/ATtiny84_Code_2/i2c_eeprom.h
@@ -7,5 +7,6 @@ void EEPROM_write(unsigned char chip_addr, unsigned int addr, unsigned char* dat
 void EEPROM_read(unsigned char chip_addr, unsigned int addr, unsigned char* data, unsigned int data_size);
 void EEPROM_init(void);
 void I2C_stop(void);
+void release_I2C_Bus(void);
 
 #endif

b51a2c41b6e0e2a3bb191ea26a9fb200b3801af4
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2018-11-17 12:23:13 -0700

    Update RTC_PCF8523.cpp

--- src/piggy_back_loggers/SD_prototype_Firmware/Firmware/RTC_PCF8523.cpp
+++ src/piggy_back_loggers/SD_prototype_Firmware/Firmware/RTC_PCF8523.cpp
@@ -93,25 +93,25 @@ void loadDateTime(Date_t* Date)
   byte minute1  = minutes & 0x0F;
   Date->minutes = ((minute10 * 10) + minute1);
   
-  byte hours    = rtcTransfer(reg_Hours,   READ, 0);
+  byte hours    = rtcTransfer(reg_Hours, READ, 0);
   byte hour10   = hours >> 4;
   byte hour1    = hours & 0x0F;
   Date->hours   = ((hour10 * 10) + hour1);
   
-  byte days     = rtcTransfer(reg_Days,    READ, 0);
+  byte days     = rtcTransfer(reg_Days, READ, 0);
   byte day10    = days >> 4;
   byte day1     = days & 0x0F;
   Date->days    = ((day10 * 10) + day1);
   
-  byte months   = rtcTransfer(reg_Months,  READ, 0);
+  byte months   = rtcTransfer(reg_Months, READ, 0);
   byte month10  = months >> 4;
   byte month1   = months & 0x0F;
   Date->months  = ((month10 * 10) + month1);
   
-  byte years    = rtcTransfer(reg_Years,   READ, 0);
+  byte years    = rtcTransfer(reg_Years, READ, 0);
   byte year10   = years >> 4;
   byte year1    = years & 0x0F;
   Date->years   = ((year10 * 10) + year1);
-  
+
   return;
 }

9e4f750f7a7b8e7172279279241c85cfb4f15e44
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2019-01-26 15:40:49 -0700

    Create handleSerial.h

---
+++ src/piggy_back_loggers/SD_Mag_Prototype/handleSerial.h
@@ -0,0 +1,27 @@
+#ifndef HANDLESERIAL_H
+#define HANDLESERIAL_H
+
+#include <Arduino.h>
+#include <DebugMacros.h>
+#include <LSM303CTypes.h>
+#include <SparkFunIMU.h>
+#include <SparkFunLSM303C.h>
+
+#include "state.h"
+#include "RTC_PCF8523.h"
+
+void handleSerial(volatile State_t* State, Date_t* Date, volatile SignalState_t* SignalState, LSM303C* mag);
+void cleanSD(volatile State_t* State);
+void viewDateTime(Date_t* Date);
+void exitSerial(volatile State_t* State, Date_t* Date);
+void ejectSD(volatile State_t* State);
+void printHelp();
+void initSD(volatile State_t* State);
+void startLogging(volatile State_t* State, LSM303C* mag, volatile SignalState_t* SignalState);
+void stopLogging(volatile State_t* State);
+void updateDateTime(Date_t* Date);
+void RTC_Doctor();
+char getInput();
+char getNestedInput();
+
+#endif

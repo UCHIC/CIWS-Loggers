55461b227789f13f84fcec2922b31647783479e7
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2019-01-26 15:41:32 -0700

    Create magnetometer.cpp

---
+++ src/piggy_back_loggers/SD_Mag_Prototype/magnetometer.cpp
@@ -0,0 +1,69 @@
+#include "magnetometer.h"
+#include <Arduino.h>
+#include <DebugMacros.h>
+#include <LSM303CTypes.h>
+#include <SparkFunIMU.h>
+#include <SparkFunLSM303C.h>
+#include "powerSleep.h"
+#include "state.h"
+
+/*********************************************************************\
+ * 
+ * Library for interfacing with the SparkFun LSM303C magnetometer.
+ * 
+ * Functions:
+ *    Initialize Magnetomter 
+ *    Read Data
+ *    
+\*********************************************************************/
+
+void magnetometerInit(LSM303C *mag)
+{
+  twiPowerUp();
+  
+  if (mag->begin(
+    MODE_I2C,
+    MAG_DO_20_Hz,
+    MAG_FS_8_Ga,
+    MAG_BDU_DISABLE,
+    MAG_OMXY_LOW_POWER,
+    MAG_OMZ_MEDIUM_PERFORMANCE,
+    MAG_MD_CONTINUOUS,
+    ACC_FS_2g,
+    ACC_BDU_DISABLE,
+    ACC_DISABLE_ALL,
+    ACC_ODR_POWER_DOWN
+    ) != IMU_SUCCESS)
+    {
+      Serial.println("Magnetometer setup failed. Check connection and reset.");
+      while(1);
+    }
+    twiPowerDown();
+
+    return;
+}
+
+void readData(LSM303C* mag, volatile SignalState_t* SignalState)
+{
+  twiPowerUp();
+  SignalState->x[0] = SignalState->x[1];
+  SignalState->x[1] = mag->readMagZ();
+  twiPowerDown();
+  
+  return;
+}
+
+
+void initializeData(LSM303C* mag, volatile SignalState_t* SignalState)
+{
+  twiPowerUp();
+  SignalState->x[0] = mag->readMagZ();
+  SignalState->x[1] = SignalState->x[0];
+  SignalState->x_max = SignalState->x[1];
+  SignalState->x_min = SignalState->x[1];
+  SignalState->currentMax = SignalState->x[1];
+  SignalState->currentMin = SignalState->x[1];
+  twiPowerDown();
+  
+  return;
+}

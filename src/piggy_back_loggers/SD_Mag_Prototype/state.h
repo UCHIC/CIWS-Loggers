2e391bd6b1ed5c8b01446e32eafabf04352c7330
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2019-01-26 15:47:21 -0700

    Create state.h

---
+++ src/piggy_back_loggers/SD_Mag_Prototype/state.h
@@ -0,0 +1,38 @@
+#ifndef STATE_H
+#define STATE_H
+
+#include <Arduino.h>
+
+// State Struct
+
+typedef struct
+{
+  volatile byte pulseCount;
+  volatile unsigned long recordNum;
+  volatile bool logging;
+  volatile bool flag4;
+  volatile bool serialOn;
+  volatile bool SDin;
+  volatile bool readMag;
+  const char filename[12] = "datalog.csv";
+} State_t;
+
+// Signal State Struct
+
+typedef struct
+{
+  volatile float x[2];
+  volatile float currentMax;
+  volatile float currentMin;
+  volatile float x_max;
+  volatile float x_min;
+  volatile float offset = 0;
+  bool slopeWasPositive = false;
+  bool slopeIsPositive = false;
+  bool slopeWasNegative = false;
+  bool slopeIsNegative = false;
+} SignalState_t;
+
+void resetState(volatile State_t* State);
+
+#endif

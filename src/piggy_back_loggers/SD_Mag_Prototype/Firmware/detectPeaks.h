a7a6d69027b622a791d30f6d837a139541585103
tracy2995 <39059499+tracy2995@users.noreply.github.com>
2019-01-28 11:48:29 -0700

    Update detectPeaks.h
    Updated comments, added a TODO

--- src/piggy_back_loggers/SD_Mag_Prototype/Firmware/detectPeaks.h
+++ src/piggy_back_loggers/SD_Mag_Prototype/Firmware/detectPeaks.h
@@ -4,6 +4,8 @@
 #include <Arduino.h>
 #include "state.h"
 
-bool peakDetected(volatile SignalState_t* signalState);
+/*TODO: Add a calibration period to ensure noise is ignored.*/
+
+bool peakDetected(volatile SignalState_t* signalState); // Used to detect if a peak has occured. Attempts to filter noise.
 
 #endif

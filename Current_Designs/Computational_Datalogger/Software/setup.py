6f7268b43caa095b6821f29a1bdf7a7ed4e693ed
tracy2995 <joshuatracy64@gmail.com>
2019-08-14 14:52:43 -0600

    Changed setup.py to support inclusion of libraries in creating the Logger module. Needs to be tested.

--- Current_Designs/Computational_Datalogger/Software/setup.py
+++ Current_Designs/Computational_Datalogger/Software/setup.py
@@ -1,3 +1,10 @@
 from distutils.core import setup, Extension
+
+loggerModule = Extension('Logger',
+			 include_dirs = ['/usr/include'],
+			 libraries = ['wiringPi'],
+			 library_dirs = ['/usr/lib'],
+			 sources = ['logger.c'])
+
 setup(name = 'Logger', version = '1.0', ext_modules = [Extension('Logger', ['logger.c'])])
 

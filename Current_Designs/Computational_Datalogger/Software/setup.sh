123a976195fc002822630e316f72a24e02ecd125
tracy2995 <joshuatracy64@gmail.com>
2019-08-15 11:21:20 -0600

    Updated setup.sh to set up the Raspberry Pi for UART Communication

--- Current_Designs/Computational_Datalogger/Software/setup.sh
+++ Current_Designs/Computational_Datalogger/Software/setup.sh
@@ -3,12 +3,44 @@
 # Dependancies: python-dev
 #		wiringpi
 
+# TODO: need to setup UART. UART setup is a bit funky.
+#	1) Add enable_uart=1 to /boot/config.txt
+#	2) Add core_freq=250 to /boot/config.txt (or whatever core frequency we wanna run at)
+#	3) sudo systemctl stop serial-getty@ttyS0.service
+#	4) sudo systemctl disable serial-getty@ttyS0.service
+#	5) Remove console=serial0,115200 from /boot/cmdline.txt
+
 echo "Checking /boot/cmdline.txt for spidev buffer size..."
 
 grep -q -F 'spidev.bufsiz=65536' /boot/cmdline.txt
 if [ $? -ne 0 ]; then
 	ed -s /boot/cmdline.txt < <(printf '%s\n' 1 a " spidev.bufsiz=65536" . 1,2j w q)
 	echo "Spidev buffer size added. Reboot required in order to complete setup."
+fi
+
+echo "Checking /boot/cmdline.txt for enabled serial console..."
+
+grep -q -F 'console=serial0,115200' /boot/cmdline.txt
+if [ $? -ne 0 ]; then
+	sed 's/console=serial0,115200//g' /boot/cmdline.txt
+	echo "Disabled Serial Console. Reboot required in order to complete setup."
+fi
+
+echo "Checking /etc/rc.local to disable serial-getty service..."
+
+grep -q -F 'sudo systemctl disable serial-getty@ttyS0.service' /etc/rc.local
+if [ $? -ne 0 ]; then
+	echo 'sudo systemctl stop serial-getty@ttyS0.service' >> /etc/rc.local
+	echo 'sudo systemctl disable serial-getty@ttyS0.service' >> /etc/rc.local
+	echo "Disabled serial-getty service. Reboot required in order to complete setup."
+fi
+
+echo "Checking /boot/config.txt for enabled UART..."
+
+grep -q -F 'enable_uart=1' /boot/config.txt
+if [ $? -ne 0 ]; then
+	echo 'enable_uart=1' /boot/config.txt
+	echo 'core_freq=250' /boot/config.txt
 fi
 
 echo "Building module Logger"
